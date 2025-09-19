cfe219b671b5cd0b0863bf413f5ebda909b7e63e
Marco Perez <mperez154@gmail.com>
2025-09-19 13:20:52 -0400

    Added routing for Stock History, Weather

--- samplecode.client/src/app/app-routing.module.ts
+++ samplecode.client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StockHistoryComponent } from './stock-history/stock-history.component'
+import { WeatherComponent } from './weather/weather.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stock-history', component: StockHistoryComponent },
+  { path: 'weather', component: WeatherComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- samplecode.client/src/app/app.module.ts
+++ samplecode.client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StockHistoryComponent } from './stock-history/stock-history.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockHistoryComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
