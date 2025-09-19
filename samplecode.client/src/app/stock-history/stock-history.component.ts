364aa2ee6920dc49a983c6f91478150632086d4d
Marco Perez <mperez154@gmail.com>
2025-09-19 13:19:06 -0400

    Added Stock History, Weather page

---
+++ samplecode.client/src/app/stock-history/stock-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stock-history',
+  standalone: false,
+  templateUrl: './stock-history.component.html',
+  styleUrl: './stock-history.component.css'
+})
+export class StockHistoryComponent {
+
+}
